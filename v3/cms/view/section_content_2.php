ad7916f5b2a68010ef4227fddb3950555305960a
LisPrzemyslaw84@gmail.com <LisPrzemyslaw84@gmail.com@4a040ae9-2b6f-9a66-eb97-42316f4a3ef1>
2012-03-11 17:25:51 +0000

    -HTML preview tooltip width limit removed

--- v3/cms/view/section_content_2.php
+++ v3/cms/view/section_content_2.php
@@ -18,21 +18,25 @@
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-if (!isset($ini))
-{
+if (!isset($ini)) {
     require_once'../../Ini.php';
     $ini = new Ini();
 }
 $logged_user = User::get_logged_user();
-if ($logged_user == null)
-{
+if ($logged_user == null) {
     echo "<script>location.reload();</script>";
     die(Language::string(278));
 }
-
+?>
+<script>
+    $(function(){
+        $(".tooltipHTMLPreview").tooltip({
+        });
+    })
+</script>
+<?php
 $vals = $_POST['value'];
-if (array_key_exists('oid', $_POST) && $_POST['oid'] != 0)
-{
+if (array_key_exists('oid', $_POST) && $_POST['oid'] != 0) {
     $section = TestSection::from_mysql_id($_POST['oid']);
     $vals = $section->get_values();
 }
@@ -45,50 +49,48 @@ if (array_key_exists('oid', $_POST) && $_POST['oid'] != 0)
 $preview = Language::string(213);
 
 $template = Template::from_mysql_id($vals[0]);
-if ($vals[0] != 0 && $template != null)
-{
+if ($vals[0] != 0 && $template != null) {
     ?>
     <b><?= Language::string(106) ?>:</b>
     <div class="ui-widget-content ui-state-focus">
         <div>
             <table>
-                <?php
-                $preview.=" " . Language::string(214) . ":<hr/>" . $template->HTML;
+    <?php
+    $preview.=" " . Language::string(214) . ":<hr/>" . $template->HTML;
 
-                $inserts = $template->get_inserts();
+    $inserts = $template->get_inserts();
 
-                $j = 0;
-                for ($i = 0; $i < count($inserts); $i++)
-                {
-                    $is_special = false;
-                    if ($inserts[$i] == "TIME_LEFT") $is_special = true;
+    $j = 0;
+    for ($i = 0; $i < count($inserts); $i++) {
+        $is_special = false;
+        if ($inserts[$i] == "TIME_LEFT")
+            $is_special = true;
 
-                    if (!$is_special)
-                    {
-                        $val = $inserts[$i];
-                        if (isset($vals[3 + $j]) && $vals[3 + $j] != "")
-                                $val = $vals[3 + $j];
-                        $j++;
-                    }
-                    ?>
+        if (!$is_special) {
+            $val = $inserts[$i];
+            if (isset($vals[3 + $j]) && $vals[3 + $j] != "")
+                $val = $vals[3 + $j];
+            $j++;
+        }
+        ?>
                     <tr>
                         <td><span class="spanIcon ui-icon ui-icon-help tooltip" title="<?= Language::string(215) ?>"></span></td>
                         <td><?= $inserts[$i] ?></td>
                         <td>
-                            <?php
-                            if (!$is_special)
-                            {
-                                ?>    
-                                <?= Language::string(279) ?> <input type="text" class="controlValue<?= $_POST['counter'] ?>_params ui-widget-content ui-corner-all comboboxVars" value="<?= htmlspecialchars($val, ENT_QUOTES) ?>" />
-                                <?php
-                            }
-                            else echo "&nbsp;";
-                            ?>
+                    <?php
+                    if (!$is_special) {
+                        ?>    
+            <?= Language::string(279) ?> <input type="text" class="controlValue<?= $_POST['counter'] ?>_params ui-widget-content ui-corner-all comboboxVars" value="<?= htmlspecialchars($val, ENT_QUOTES) ?>" />
+            <?php
+        }
+        else
+            echo "&nbsp;";
+        ?>
                         </td>
                     </tr>
-                    <?php
-                }
-                ?>
+                            <?php
+                        }
+                        ?>
                 <tr>
                     <td><span class="spanIcon ui-icon ui-icon-help tooltip" title="<?= Language::string(216) ?>"></span></td>
                     <td>TIME_LIMIT</td>
@@ -97,37 +99,35 @@ if ($vals[0] != 0 && $template != null)
             </table>
         </div>
         <div class="notVisible">
-            <?php
-            for ($i = 0; $i < count($inserts); $i++)
-            {
-                ?>
+    <?php
+    for ($i = 0; $i < count($inserts); $i++) {
+        ?>
                 <input class="inputParameterVar" type="hidden" value="<?= $inserts[$i] ?>" />
-                <?php
-            }
-            ?>
+        <?php
+    }
+    ?>
             <input class="inputParameterVar" type="hidden" value="TIME_LIMIT" />
         </div>
     </div>
     <br/>
-    <?php
-}
-?>
+            <?php
+        }
+        ?>
 <b>choose the HTML template:</b>
 <table>
     <tr>
         <td>
-            <span class="spanIcon ui-icon ui-icon-help tooltip" title="<?= htmlspecialchars($preview, ENT_QUOTES) ?>"></span>
+            <span class="spanIcon ui-icon ui-icon-help tooltipHTMLPreview" title="<?= htmlspecialchars($preview, ENT_QUOTES) ?>"></span>
         </td>
         <td>
             <select id="selectTemplate_<?= $_POST['counter'] ?>" class="fullWidth ui-widget-content ui-corner-all" onchange="Test.uiTemplatesChanged()">
                 <option value="0">&lt;from CURRENT_TEMPLATE_ID&gt;</option>
-                <?php
-                $sql = $logged_user->mysql_list_rights_filter("Template", "`name` ASC");
-                $z = mysql_query($sql);
-                while ($r = mysql_fetch_array($z))
-                {
-                    $t = Template::from_mysql_id($r[0]);
-                    ?>
+<?php
+$sql = $logged_user->mysql_list_rights_filter("Template", "`name` ASC");
+$z = mysql_query($sql);
+while ($r = mysql_fetch_array($z)) {
+    $t = Template::from_mysql_id($r[0]);
+    ?>
                     <option value="<?= $t->id ?>" <?= ($vals[0] == $t->id ? "selected" : "") ?>><?= $t->name ?> ( <?= $t->get_system_data() ?> )</option>
                 <?php } ?>
             </select>
@@ -135,30 +135,28 @@ if ($vals[0] != 0 && $template != null)
     </tr>
 </table>
 
-<?php
-if ($vals[0] != 0 && $template != null)
-{
-    ?>
+                <?php
+                if ($vals[0] != 0 && $template != null) {
+                    ?>
 
     <b><?= Language::string(113) ?>:</b>
     <div class="ui-widget-content ui-state-focus">
         <div>
             <table>
-                <?php
-                $outputs = $template->get_outputs();
+    <?php
+    $outputs = $template->get_outputs();
 
-                for ($i = 0; $i < count($outputs); $i++)
-                {
-                    $ret = $outputs[$i]["name"];
-                    if (isset($vals[$vals[1] + 3 + $i * 3]) && $vals[$vals[1] + 3 + $i * 3] != "")
-                            $ret = $vals[$vals[1] + 3 + $i * 3];
-                    $vis = 2;
-                    if (isset($vals[$vals[1] + 3 + $i * 3 + 1]))
-                            $vis = $vals[$vals[1] + 3 + $i * 3 + 1];
-                    $type = 0;
-                    if (isset($vals[$vals[1] + 3 + $i * 3 + 2]))
-                            $type = $vals[$vals[1] + 3 + $i * 3 + 2];
-                    ?>
+    for ($i = 0; $i < count($outputs); $i++) {
+        $ret = $outputs[$i]["name"];
+        if (isset($vals[$vals[1] + 3 + $i * 3]) && $vals[$vals[1] + 3 + $i * 3] != "")
+            $ret = $vals[$vals[1] + 3 + $i * 3];
+        $vis = 2;
+        if (isset($vals[$vals[1] + 3 + $i * 3 + 1]))
+            $vis = $vals[$vals[1] + 3 + $i * 3 + 1];
+        $type = 0;
+        if (isset($vals[$vals[1] + 3 + $i * 3 + 2]))
+            $type = $vals[$vals[1] + 3 + $i * 3 + 2];
+        ?>
                     <tr>
                         <td><span class="spanIcon ui-icon ui-icon-help tooltip" title="<?= Language::string(217) ?>: <b><?= $outputs[$i]["type"] ?></b>"></span></td>
                         <td><?= $outputs[$i]["name"] ?></td>
@@ -178,9 +176,9 @@ if ($vals[0] != 0 && $template != null)
                             </select>
                         </td>
                     </tr>
-                    <?php
-                }
-                ?>
+        <?php
+    }
+    ?>
                 <tr>
                     <td><span class="spanIcon ui-icon ui-icon-help tooltip" title="<?= Language::string(283) ?>"></span></td>
                     <td>CURRENT_TEMPLATE_ID</td>
@@ -194,18 +192,17 @@ if ($vals[0] != 0 && $template != null)
             </table> 
         </div>
         <div class="notVisible">
-            <?php
-            for ($i = 0; $i < count($outputs); $i++)
-            {
-                ?>
+    <?php
+    for ($i = 0; $i < count($outputs); $i++) {
+        ?>
                 <input class="inputReturnVar" type="hidden" value="<?= $outputs[$i]['name'] ?>" />
-                <?php
-            }
-            ?>
+        <?php
+    }
+    ?>
             <input class="inputReturnVar" type="hidden" value="CURRENT_TEMPLATE_ID" />
             <input class="inputReturnVar" type="hidden" value="LAST_PRESSED_BUTTON_NAME" />
         </div>
     </div>
-    <?php
-}
-?>
+            <?php
+        }
+        ?>
