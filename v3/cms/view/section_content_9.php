182183ef7a63f46c85b736950213577d23832c26
LisPrzemyslaw84@gmail.com <LisPrzemyslaw84@gmail.com@4a040ae9-2b6f-9a66-eb97-42316f4a3ef1>
2012-03-13 17:28:39 +0000

    -added default reference in custom section values

--- v3/cms/view/section_content_9.php
+++ v3/cms/view/section_content_9.php
@@ -71,7 +71,7 @@ if (count($parameters) > 0)
                     <tr>
                         <td><span class="spanIcon ui-icon ui-icon-help tooltip" title="<?= htmlspecialchars($parameters[$i]->description, ENT_QUOTES) ?>"></span></td>
                         <td><?= $parameters[$i]->name ?></td>
-                        <td><b><?= Language::string(279) ?></b> <input type="text" class="controlValue<?= $_POST['counter'] ?> ui-widget-content ui-corner-all comboboxVars" value="<?= htmlspecialchars(isset($vals[$j]) ? $vals[$j] : "", ENT_QUOTES) ?>" /></td>
+                        <td><b><?= Language::string(279) ?></b> <input type="text" class="controlValue<?= $_POST['counter'] ?> ui-widget-content ui-corner-all comboboxVars" value="<?= htmlspecialchars(isset($vals[$j]) ? $vals[$j] : $parameters[$i]->name, ENT_QUOTES) ?>" /></td>
                     </tr>
                     <?php
                     $j++;
@@ -98,7 +98,7 @@ if (count($returns) > 0)
                     <tr>
                         <td><span class="spanIcon ui-icon ui-icon-help tooltip" title="<?= htmlspecialchars($returns[$i]->description, ENT_QUOTES) ?>"></span></td>
                         <td><?= $returns[$i]->name ?></td>
-                        <td><?= Language::string(279) ?> <input onchange="Test.uiSetVarNameChanged($(this))" type="text" class="ui-state-focus comboboxSetVars comboboxVars controlValue<?= $_POST['counter'] ?> ui-widget-content ui-corner-all" value="<?= htmlspecialchars(isset($vals[$j]) ? $vals[$j] : "", ENT_QUOTES) ?>" /></td>
+                        <td><?= Language::string(279) ?> <input onchange="Test.uiSetVarNameChanged($(this))" type="text" class="ui-state-focus comboboxSetVars comboboxVars controlValue<?= $_POST['counter'] ?> ui-widget-content ui-corner-all" value="<?= htmlspecialchars(isset($vals[$j]) ? $vals[$j] : $returns[$i]->name, ENT_QUOTES) ?>" /></td>
                         <td>
                             <b><?= Language::string(277) ?></b> <select class="controlValue<?= $_POST['counter'] ?> ui-widget-content ui-corner-all">
                                 <option value="0" <?= ($vals[$j + 1] == 0 ? "selected" : "") ?>><?= Language::string(275) ?></option>
