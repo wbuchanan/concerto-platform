ef895b7b1e0434dd5476af9d4cc931d20bcad718
LisPrzemyslaw84@gmail.com <LisPrzemyslaw84@gmail.com@4a040ae9-2b6f-9a66-eb97-42316f4a3ef1>
2012-04-30 16:18:21 +0000

    -template preview fix -french translation implemented

--- v3/cms/model/Template.php
+++ v3/cms/model/Template.php
@@ -19,71 +19,77 @@
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-class Template extends OModule {
-
+class Template extends OModule
+{
     public $name = "";
     public $HTML = "";
     public static $exportable = true;
     public static $mysql_table_name = "Template";
 
-    public function __construct($params = array()) {
+    public function __construct($params = array())
+    {
         $this->name = Language::string(75);
         parent::__construct($params);
     }
 
-    public function get_inserts() {
+    public function get_inserts()
+    {
         $inserts = array();
         $html = $this->HTML;
-        while (strpos($html, "{{") !== false) {
+        while (strpos($html, "{{") !== false)
+        {
             $html = substr($html, strpos($html, "{{") + 2);
-            if (strpos($html, "}}") !== false) {
+            if (strpos($html, "}}") !== false)
+            {
                 $name = substr($html, 0, strpos($html, "}}"));
-                if ($name == "TIME_LEFT")
-                    continue;
-                if (!in_array($name, $inserts))
-                    array_push($inserts, $name);
+                if ($name == "TIME_LEFT") continue;
+                if (!in_array($name, $inserts)) array_push($inserts, $name);
             }
         }
         return $inserts;
     }
 
-    public function get_insert_reference($name, $vals) {
+    public function get_insert_reference($name, $vals)
+    {
         $inserts = $this->get_inserts();
         $j = 3;
-        foreach ($inserts as $ins) {
-            if ($ins == "TIME_LEFT")
-                continue;
+        foreach ($inserts as $ins)
+        {
+            if ($ins == "TIME_LEFT") continue;
 
-            if ($ins == $name)
-                return $vals[$j];
+            if ($ins == $name) return $vals[$j];
 
             $j++;
         }
         return $name;
     }
 
-    public function get_return_reference($name, $vals, $output = null) {
-        if ($output == null)
-            $output = $this->get_outputs();
+    public function get_return_reference($name, $vals, $output = null)
+    {
+        if ($output == null) $output = $this->get_outputs();
         $j = 3 + $vals[1];
-        foreach ($output as $ret) {
-            if ($ret["name"] == $name)
-                return $vals[$j];
+        foreach ($output as $ret)
+        {
+            if ($ret["name"] == $name) return $vals[$j];
 
             $j++;
         }
         return $name;
     }
 
-    public function get_html_with_return_properties($vals) {
+    public function get_html_with_return_properties($vals)
+    {
         $html = str_get_html($this->HTML);
         $outputs = $this->get_outputs();
 
-        foreach ($outputs as $out) {
+        foreach ($outputs as $out)
+        {
             $elems = $html->find("[name='" . $out["name"] . "']");
             $reference = null;
-            foreach ($elems as $elem) {
-                if ($reference == null) {
+            foreach ($elems as $elem)
+            {
+                if ($reference == null)
+                {
                     $reference = $this->get_return_reference($out["name"], $vals, $outputs);
                 }
                 $elem->setAttribute("name", $reference);
@@ -92,22 +98,26 @@ class Template extends OModule {
         return $html->save();
     }
 
-    public function get_outputs() {
+    public function get_outputs()
+    {
         $names = array();
         $outputs = array();
         $html_string = $this->HTML;
-        if (empty($html_string))
-            $html_string = "<p></p>";
+        if (empty($html_string)) $html_string = "<p></p>";
         $html = str_get_html($html_string);
-        foreach ($html->find('input[type="text"], input[type="password"], input[type="checkbox"], input[type="radio"]') as $element) {
-            if (!in_array($element->name, $names)) {
+        foreach ($html->find('input[type="text"], input[type="password"], input[type="checkbox"], input[type="radio"]') as $element)
+        {
+            if (!in_array($element->name, $names))
+            {
                 array_push($outputs, array("name" => $element->name, "type" => $element->type));
                 array_push($names, $element->name);
             }
         }
 
-        foreach ($html->find('textarea, select') as $element) {
-            if (!in_array($element->name, $names)) {
+        foreach ($html->find('textarea, select') as $element)
+        {
+            if (!in_array($element->name, $names))
+            {
                 array_push($outputs, array("name" => $element->name, "type" => $element->tag));
                 array_push($names, $element->name);
             }
@@ -115,7 +125,8 @@ class Template extends OModule {
         return $outputs;
     }
 
-    public function export() {
+    public function export()
+    {
         $xml = new DOMDocument('1.0', "UTF-8");
         $xml->preserveWhiteSpace = false;
         $xml->formatOutput = true;
@@ -134,19 +145,22 @@ class Template extends OModule {
         return trim($xml->saveXML());
     }
 
-    public function import($path) {
+    public function import($path)
+    {
         $xml = new DOMDocument('1.0', 'UTF-8');
-        if (!$xml->load($path))
-            return -4;
+        if (!$xml->load($path)) return -4;
 
         $this->Sharing_id = 1;
 
         $xpath = new DOMXPath($xml);
         $elements = $xpath->query("/export/Templates/Template");
-        foreach ($elements as $element) {
+        foreach ($elements as $element)
+        {
             $children = $element->childNodes;
-            foreach ($children as $child) {
-                switch ($child->nodeName) {
+            foreach ($children as $child)
+            {
+                switch ($child->nodeName)
+                {
                     case "name": $this->name = $child->nodeValue;
                         break;
                     case "HTML": $this->HTML = $child->nodeValue;
@@ -157,7 +171,8 @@ class Template extends OModule {
         return $this->mysql_save();
     }
 
-    public function to_XML() {
+    public function to_XML()
+    {
         $xml = new DOMDocument('1.0', 'UTF-8');
 
         $element = $xml->createElement("Template");
@@ -175,10 +190,11 @@ class Template extends OModule {
         return $element;
     }
 
-    public static function create_db($delete = false) {
-        if ($delete) {
-            if (!mysql_query("DROP TABLE IF EXISTS `Template`;"))
-                return false;
+    public static function create_db($delete = false)
+    {
+        if ($delete)
+        {
+            if (!mysql_query("DROP TABLE IF EXISTS `Template`;")) return false;
         }
         $sql = "
             CREATE TABLE IF NOT EXISTS `Template` (
@@ -195,29 +211,36 @@ class Template extends OModule {
         return mysql_query($sql);
     }
 
-    public static function strip_html($html, $scripts = true, $styles = true) {
-        $html = new simple_html_dom();
-        $html->load($html);
-        if ($styles) {
-            $elems = $html->find("style");
-            foreach ($elems as $elem) {
+    public static function strip_html($html, $scripts = true, $styles = true)
+    {
+        $obj = new simple_html_dom();
+        $obj->load($html);
+        if ($styles)
+        {
+            $elems = $obj->find("style");
+            foreach ($elems as $elem)
+            {
                 $elem->outertext = "";
             }
-            $elems = $html->find("link");
-            foreach ($elems as $elem) {
+            $elems = $obj->find("link");
+            foreach ($elems as $elem)
+            {
                 $elem->outertext = "";
             }
         }
-        if ($scripts) {
-            $elems = $html->find("script");
-            foreach ($elems as $elem) {
+        if ($scripts)
+        {
+            $elems = $obj->find("script");
+            foreach ($elems as $elem)
+            {
                 $elem->outertext = "";
             }
         }
-        return $html->save();
+        return $obj->save();
     }
 
-    public function get_preview_HTML() {
+    public function get_preview_HTML()
+    {
         return Template::strip_html($this->HTML);
     }
 
