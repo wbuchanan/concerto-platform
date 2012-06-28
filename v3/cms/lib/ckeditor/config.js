15caef337ac0ffcb7ff31cd9370e711d41a03d3d
LisPrzemyslaw84@gmail.com <LisPrzemyslaw84@gmail.com@4a040ae9-2b6f-9a66-eb97-42316f4a3ef1>
2012-06-28 18:50:41 +0000

    added audio tag plugin to CKEditor

--- v3/cms/lib/ckeditor/config.js
+++ v3/cms/lib/ckeditor/config.js
@@ -8,6 +8,28 @@ CKEDITOR.editorConfig = function( config )
     config.width = 800;
     config.extraPlugins = 'codemirror,autogrow';
     config.removePlugins = 'resize,maximize';
+    config.extraPlugins='audio'; 
+    
+    config.toolbar = 'Full';
+ 
+    config.toolbar_Full =
+    [
+            { name: 'document', items : [ 'Source','-','Save','NewPage','DocProps','Preview','Print','-','Templates' ] },
+            { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
+            { name: 'editing', items : [ 'Find','Replace','-','SelectAll','-','SpellChecker', 'Scayt' ] },
+            { name: 'forms', items : [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 
+            'HiddenField' ] },
+            '/',
+            { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
+            { name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','CreateDiv',
+            '-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ] },
+            { name: 'links', items : [ 'Link','Unlink','Anchor' ] },
+            { name: 'insert', items : [ 'Image','Audio','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak','Iframe' ] },
+            '/',
+            { name: 'styles', items : [ 'Styles','Format','Font','FontSize' ] },
+            { name: 'colors', items : [ 'TextColor','BGColor' ] },
+            { name: 'tools', items : [ 'Maximize', 'ShowBlocks','-','About' ] }
+    ];
 };
 
 CKEDITOR.plugins.load('pgrfilemanager');
