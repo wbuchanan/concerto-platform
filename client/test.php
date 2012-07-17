a827572d96155b63368572adfe9bf6127095a575
LisPrzemyslaw84@gmail.com <LisPrzemyslaw84@gmail.com@4a040ae9-2b6f-9a66-eb97-42316f4a3ef1>
2012-07-17 16:50:19 +0000

    added Concerto client test page

---
+++ client/test.php
@@ -0,0 +1,75 @@
+<?php
+/*
+  Concerto Platform - Online Adaptive Testing Platform
+  Copyright (C) 2011-2012, The Psychometrics Centre, Cambridge University
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; version 2
+  of the License, and not any of the later versions.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="description" content="" />
+        <meta name="author" content="Przemyslaw Lis" />
+        <meta http-equiv="Cache-Control" content="no-cache"/>
+        <meta http-equiv="Expires" content="-1"/>
+        <title>Concerto</title>
+
+        <script type="text/javascript" src="client/jquery-1.7.1.min.js"></script>
+        <script type="text/javascript" src="client/jquery.json-2.3.min.js"></script>
+
+        <script type="text/javascript" src="client/ConcertoMethods.js"></script>
+        <script type="text/javascript" src="client/Concerto.js"></script>
+        <script type="text/javascript" src="client/concerto.jquery.js"></script>
+        
+        <script>
+            function start(){
+                $("#divTestContainer").concerto({
+                    testID:$("#testID").val(),
+                    loadingImageSource:"client/css/img/ajax-loader.gif",
+                    callback:function(sessionID,sessionHash, status, finished){
+                        $("#log").html("<b>start</b> - sessionID: "+sessionID+", sessionHash: "+sessionHash+", status: "+status+", finished: "+finished);
+                    }});
+            }
+            
+            function resume(){
+                $("#divTestContainer").concerto({
+                    sessionID:$("#sessionID").val(),
+                    sessionHash:$("#sessionHash").val(),
+                    loadingImageSource:"client/css/img/ajax-loader.gif",
+                    callback:function(sessionID,sessionHash, status, finished){
+                        $("#log").html("<b>resume</b> - sessionID: "+sessionID+", sessionHash: "+sessionHash+", status: "+status+", finished: "+finished);
+                    }});
+            }
+        </script>
+    </head>
+
+    <body>
+        <div id="log">
+            
+        </div>
+        <div>
+            testID: <input type="text" id="testID" />, 
+            sessionID: <input type="text" id="sessionID" />
+            sessionHash: <input type="text" id="sessionHash" />
+        </div>
+        <div>
+            <button onclick="start()">start new</button><button onclick="resume()">resume</button>
+        </div>
+        <div style="width:100%;" id="divTestContainer"></div>
+    </body>
+</html>
