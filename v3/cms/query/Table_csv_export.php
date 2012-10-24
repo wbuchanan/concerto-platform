5200bde3ebd709c6f0c82a6ee656b8f1b255a953
LisPrzemyslaw84@gmail.com <LisPrzemyslaw84@gmail.com@4a040ae9-2b6f-9a66-eb97-42316f4a3ef1>
2012-10-24 22:34:53 +0000

    large table export to csv fix

--- v3/cms/query/Table_csv_export.php
+++ v3/cms/query/Table_csv_export.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
   Concerto Platform - Online Adaptive Testing Platform
   Copyright (C) 2011-2012, The Psychometrics Centre, Cambridge University
@@ -18,52 +19,46 @@
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-if (!isset($ini))
-{
+if (!isset($ini)) {
     require_once '../../Ini.php';
     $ini = new Ini();
 }
 $logged_user = User::get_logged_user();
-if ($logged_user == null) header("Location: ".Ini::$path_external."cms/index.php");
+if ($logged_user == null)
+    header("Location: " . Ini::$path_external . "cms/index.php");
 
 $table = Table::from_mysql_id($_GET['oid']);
-if (!$logged_user->is_object_readable($table)) die(Language::string(81));
+if (!$logged_user->is_object_readable($table))
+    die(Language::string(81));
 
-/**
- * Generatting CSV formatted string from an array.
- * By Sergey Gurevich.
- */
-function array_to_scv($array, $header_row = true, $col_sep = ",", $row_sep = "\r\n", $qut = '"')
-{
-    if (!is_array($array) or !is_array($array[0])) return false;
-    $output = "";
-    //Header row.
-    if ($header_row)
-    {
-        foreach ($array[0] as $key => $val)
-        {
-            if (is_numeric($key)) continue;
-            //Escaping quotes.
-            $key = str_replace($qut, "$qut$qut", $key);
-            $output .= "$col_sep$qut$key$qut";
+function output_table_to_csv($table, $header_row = true, $col_sep = ",", $row_sep = "\r\n", $qut = '"') {
+    $sql = sprintf("SELECT * FROM `%s`", $table->get_table_name());
+    $z = mysql_query($sql);
+    $i = 0;
+    while ($r = mysql_fetch_array($z)) {
+        if ($i == 0 && $header_row) {
+            $output = "";
+            foreach ($r as $key => $val) {
+                if (is_numeric($key))
+                    continue;
+
+                $key = str_replace($qut, "$qut$qut", $key);
+                $output .= "$col_sep$qut$key$qut";
+            }
+            $output = substr($output, 1) . "\n";
+            echo $output;
         }
-        $output = substr($output, 1) . "\n";
-    }
-    //Data rows.
-    foreach ($array as $key => $val)
-    {
         $tmp = '';
-        foreach ($val as $cell_key => $cell_val)
-        {
-            if (is_numeric($cell_key)) continue;
-            //Escaping quotes.
+        foreach ($r as $cell_key => $cell_val) {
+            if (is_numeric($cell_key))
+                continue;
+//Escaping quotes.
             $cell_val = str_replace($qut, "$qut$qut", $cell_val);
             $tmp .= "$col_sep$qut$cell_val$qut";
         }
-        $output .= substr($tmp, 1) . $row_sep;
+        echo substr($tmp, 1) . $row_sep;
+        $i++;
     }
-
-    return $output;
 }
 
 header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
@@ -71,14 +66,6 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 header("Content-type: application/csv");
 header('Content-Disposition: attachment; filename="table_' . $_GET['oid'] . '.csv"');
 
-$rows = array();
-$sql = sprintf("SELECT * FROM `%s`", $table->get_table_name());
-$z = mysql_query($sql);
-while ($r = mysql_fetch_array($z))
-{
-    array_push($rows, $r);
-}
-
-echo array_to_scv($rows, $_GET['header'] == 1, $_GET['delimeter'], "\r\n", $_GET['enclosure']);
+output_table_to_csv($table, $_GET['header'] == 1, $_GET['delimeter'], "\r\n", $_GET['enclosure']);
 exit();
 ?>
