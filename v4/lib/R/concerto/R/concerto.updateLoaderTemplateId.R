6dcfd9f872d56bf1012e76f2b9b860eb050eb09b
LisPrzemyslaw84@gmail.com <LisPrzemyslaw84@gmail.com@4a040ae9-2b6f-9a66-eb97-42316f4a3ef1>
2013-04-24 21:55:46 +0000

    template transition effects can now be set dynamically from test logic template loader and it's transition effects can now be set dynamically from test logic

--- v4/lib/R/concerto/R/concerto.updateLoaderTemplateId.R
+++
@@ -1,7 +0,0 @@
-concerto.updateLoaderTemplateID <-
-function(loaderTemplateID) {
-  dbName <- dbEscapeStrings(concerto$db$connection,concerto$db$name)
-  sessionID <- dbEscapeStrings(concerto$db$connection,toString(concerto$sessionID))
-  loaderTemplateID <- dbEscapeStrings(concerto$db$connection,toString(loaderTemplateID))
-  dbSendQuery(concerto$db$connection, statement = sprintf("UPDATE `%s`.`TestSession` SET `loader_Template_id` = '%s' WHERE `id`=%s",dbName,loaderTemplateID,sessionID))
-}
